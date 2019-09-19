e6039ceb4cee94072735ab5a1da079d554c5aaaf
Veronika-Lisse Veneracion <veronikaveneracion@gmail.com>
2019-09-18 22:25:16 -0400

    setting up stuff

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './style/App.css';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        hellohello
+      </div>
+    );
+  }
+}
+
+
+export default App;
