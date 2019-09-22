26c31688f00b7d72cfe3e2fdc42310c0c60187bc
Veronika-Lisse Veneracion <veronikaveneracion@gmail.com>
2019-09-21 21:33:01 -0400

    adding the boxes

--- src/components/PokeCell.js
+++ src/components/PokeCell.js
@@ -3,12 +3,12 @@ import sprites from '../assets/sprites.png';
 import './style/PokeCell.css';
 
 
-const PokeCell = ({ pokeClass }) => {
+const PokeCell = ({ pokeClass,handleOnClick }) => {
     // eslint-disable-next-line 
   const { id, backgroundPosition } = pokeClass;
   const style = { backgroundImage: `url(${sprites})`, backgroundPosition };
 
-  return <button style={style} className="poke-cell"></button>
+  return <button onClick={() => handleOnClick(id)} style={style} className="poke-cell"></button>
 };
 
 export default PokeCell;
